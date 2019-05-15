75c5ab1fc6835c325487b02c03a35ff13c4459ec
tear94fall <tear94fall@naver.com>
2019-05-15 19:26:23 +0900

    데이터 베이스 연동 페이지

---
+++ dbconn.php
@@ -0,0 +1,10 @@
+<?php
+$mysql_hostname = 'localhost';
+$mysql_username = 'root';
+$mysql_password = 'root1234';
+$mysql_database = 'web_project';
+$mysql_port = '3306';
+$mysql_charset = 'utf8';
+
+$connect = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port);
+?>
